62631ea095382358c34534deb4280ba5b811c014
Anas <anastiour@gmail.com>
2022-06-29 16:18:48 +0100

    feat(login): add expiry check of 4 hours

--- .vuepress/login/helper.js
+++ .vuepress/login/helper.js
@@ -1,5 +1,8 @@
-// Do user authorization verify
+// https://github.com/TerryZ/vuepress-login
+
 export function checkAuth () {
   const auth = JSON.parse(localStorage.getItem("auth"))
-  return auth && Object.keys(auth).length
+  if (!auth) return false
+  const expired = ((new Date().getTime() - auth.time) / 100 / 60 / 60) > 4
+  return auth && Object.keys(auth).length && !expired
 }
